0eb2266a2c133c246f07d2d0b5c601309b26e04d
aerospacesoftware <69451986+aerospacesoftware@users.noreply.github.com>
2022-03-07 21:44:12 +1300

    Fix crash on PDO add

--- libEDSsharp/PDOHelper.cs
+++ libEDSsharp/PDOHelper.cs
@@ -408,7 +408,7 @@ namespace libEDSsharp
         public void addPDOslot(UInt16 configindex)
         {
             //quick range check, it must be a config index for an RXPDO or a TXPDO
-            if( (configindex<0x1400) || (configindex >= 0x1a00)  || ((configindex>=1600) && (configindex<0x1800)))
+            if( (configindex<0x1400) || (configindex >= 0x1a00)  || ((configindex>=0x1600) && (configindex<0x1800)))
                 return;
 
             foreach(PDOSlot slot in pdoslots)
@@ -422,7 +422,10 @@ namespace libEDSsharp
             PDOSlot newslot = new PDOSlot();
             newslot.ConfigurationIndex = configindex;
 
-            newslot.COB = 0x180; //Fixme need a better default???
+
+            newslot.COB = 0x180;        // Fixme need a better defaults???
+            newslot.configloc = "RAM";
+            newslot.mappingloc = "RAM";
 
             pdoslots.Add(newslot);
 
